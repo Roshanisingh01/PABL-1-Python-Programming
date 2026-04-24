f8ece885fedf81f17fe75a83658ff96cbbe81ee9
Roshanisingh01 <rs4877955@gmail.com>
2026-04-25 04:22:03 +0530

    Add combinationSum method to Solution class
    Implement backtracking algorithm for combination sum.

---
+++ backtracking.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, k, n):
+        res = []
+        
+        def backtrack(start, path, target):
+            if len(path) == k:
+                if target == 0:
+                    res.append(path[:])
+                return
+            
+            for i in range(start, 10):
+                if i > target:
+                    break
+                
+                path.append(i)
+                backtrack(i + 1, path, target - i)
+                path.pop()
+        
+        backtrack(1, [], n)
+        return res
