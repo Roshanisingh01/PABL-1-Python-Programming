1695648ac5706d0279d0eb3522869d304d5026b0
Roshanisingh01 <rs4877955@gmail.com>
2026-04-25 03:45:17 +0530

    Add sortColors function to sort an array of colors
    Implement Dutch National Flag algorithm to sort colors in-place.

---
+++ sort_colors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left = 0
+        curr = 0
+        right = len(nums) - 1
+
+        while curr <= right:
+            if nums[curr] == 0:
+                nums[left], nums[curr] = nums[curr], nums[left]
+                left += 1
+                curr += 1
+            elif nums[curr] == 1:
+                curr += 1
+            else:
+                nums[curr], nums[right] = nums[right], nums[curr]
+                right -= 1
